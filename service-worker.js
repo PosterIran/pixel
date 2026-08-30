0750013d30d74be65913bbb593e3b96d745f7eb1
PosterIran <Iranposter008@gmail.com>
2026-08-30 22:32:34 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './css/style.css?v=2.1.5',
+  './css/style.css?v=2.1.7',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   'https://cdn.tailwindcss.com',
